65a34df8e3d96671bd0fc800d9d047861dba464f
Saranomy <anti7gn@gmail.com>
2012-12-22 20:16:39 +0700

    Change Wall Color to Grey

--- src/com/cg/trashman/object/Maze.java
+++ src/com/cg/trashman/object/Maze.java
@@ -66,17 +66,45 @@ public class Maze implements ISimpleObject {
 					gl.glEnd();
 				} else {
 					gl.glLoadIdentity(); // reset the current model-view matrix
-					gl.glTranslatef(2f * i, -2f, -2f * j);
+					gl.glTranslatef(2f * i, 0f, -2f * j);
 
 					gl.glBegin(GL_QUADS); // of the color cube
 
 					// Top-face
-					float rand = 0.5f * random.nextFloat();
-					gl.glColor3f(rand, 0.5f + rand, rand); // green
+					gl.glColor3f(0.3f, 0.3f, 0.3f); // green
 					gl.glVertex3f(1.0f, 1.0f, -1.0f);
 					gl.glVertex3f(-1.0f, 1.0f, -1.0f);
 					gl.glVertex3f(-1.0f, 1.0f, 1.0f);
 					gl.glVertex3f(1.0f, 1.0f, 1.0f);
+
+					// Front-face
+					gl.glColor3f(0.4f, 0.4f, 0.4f); // red
+					gl.glVertex3f(1.0f, 1.0f, 1.0f);
+					gl.glVertex3f(-1.0f, 1.0f, 1.0f);
+					gl.glVertex3f(-1.0f, -1.0f, 1.0f);
+					gl.glVertex3f(1.0f, -1.0f, 1.0f);
+
+					// Back-face
+					gl.glColor3f(0.5f, 0.5f, 0.5f); // yellow
+					gl.glVertex3f(1.0f, -1.0f, -1.0f);
+					gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+					gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+					gl.glVertex3f(1.0f, 1.0f, -1.0f);
+
+					// Left-face
+					//gl.glColor3f(0.0f, 0.0f, 1.0f); // blue
+					gl.glVertex3f(-1.0f, 1.0f, 1.0f);
+					gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+					gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+					gl.glVertex3f(-1.0f, -1.0f, 1.0f);
+
+					// Right-face
+					//gl.glColor3f(1.0f, 0.0f, 1.0f); // magenta
+					gl.glVertex3f(1.0f, 1.0f, -1.0f);
+					gl.glVertex3f(1.0f, 1.0f, 1.0f);
+					gl.glVertex3f(1.0f, -1.0f, 1.0f);
+					gl.glVertex3f(1.0f, -1.0f, -1.0f);
+					
 					gl.glEnd();
 				}
 			}
